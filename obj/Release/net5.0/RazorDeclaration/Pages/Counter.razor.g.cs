ee100ba168a9e32b73b769e1a4dd9900a05147ba
Aaron van den Berg <a.vdberg98@gmail.com>
2021-03-18 11:14:17 +0100

    Updated .dotnet version

--- obj/Release/net5.0/RazorDeclaration/Pages/Counter.razor.g.cs
+++ obj/Release/net5.0/RazorDeclaration/Pages/Counter.razor.g.cs
@@ -13,70 +13,70 @@ namespace SocialMediaApplication.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\aaron\source\repos\SocialMediaApplication\_Imports.razor"
+#line 1 "C:\Users\aaron\RiderProjects\Blazor-Social-Media\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\aaron\source\repos\SocialMediaApplication\_Imports.razor"
+#line 2 "C:\Users\aaron\RiderProjects\Blazor-Social-Media\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\aaron\source\repos\SocialMediaApplication\_Imports.razor"
+#line 3 "C:\Users\aaron\RiderProjects\Blazor-Social-Media\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\aaron\source\repos\SocialMediaApplication\_Imports.razor"
+#line 4 "C:\Users\aaron\RiderProjects\Blazor-Social-Media\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\aaron\source\repos\SocialMediaApplication\_Imports.razor"
+#line 5 "C:\Users\aaron\RiderProjects\Blazor-Social-Media\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\aaron\source\repos\SocialMediaApplication\_Imports.razor"
+#line 6 "C:\Users\aaron\RiderProjects\Blazor-Social-Media\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\aaron\source\repos\SocialMediaApplication\_Imports.razor"
+#line 7 "C:\Users\aaron\RiderProjects\Blazor-Social-Media\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\aaron\source\repos\SocialMediaApplication\_Imports.razor"
+#line 8 "C:\Users\aaron\RiderProjects\Blazor-Social-Media\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\aaron\source\repos\SocialMediaApplication\_Imports.razor"
+#line 9 "C:\Users\aaron\RiderProjects\Blazor-Social-Media\_Imports.razor"
 using SocialMediaApplication;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\aaron\source\repos\SocialMediaApplication\_Imports.razor"
+#line 10 "C:\Users\aaron\RiderProjects\Blazor-Social-Media\_Imports.razor"
 using SocialMediaApplication.Shared;
 
 #line default
@@ -91,7 +91,7 @@ using SocialMediaApplication.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 9 "C:\Users\aaron\source\repos\SocialMediaApplication\Pages\Counter.razor"
+#line 9 "C:\Users\aaron\RiderProjects\Blazor-Social-Media\Pages\Counter.razor"
        
     private int currentCount = 0;
 
--- obj/Release/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
+++ obj/Release/net5.0/RazorDeclaration/Pages/FetchData.razor.g.cs
@@ -13,77 +13,77 @@ namespace SocialMediaApplication.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\aaron\source\repos\SocialMediaApplication\_Imports.razor"
+#line 1 "C:\Users\aaron\RiderProjects\Blazor-Social-Media\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\aaron\source\repos\SocialMediaApplication\_Imports.razor"
+#line 2 "C:\Users\aaron\RiderProjects\Blazor-Social-Media\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\aaron\source\repos\SocialMediaApplication\_Imports.razor"
+#line 3 "C:\Users\aaron\RiderProjects\Blazor-Social-Media\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\aaron\source\repos\SocialMediaApplication\_Imports.razor"
+#line 4 "C:\Users\aaron\RiderProjects\Blazor-Social-Media\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\aaron\source\repos\SocialMediaApplication\_Imports.razor"
+#line 5 "C:\Users\aaron\RiderProjects\Blazor-Social-Media\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\aaron\source\repos\SocialMediaApplication\_Imports.razor"
+#line 6 "C:\Users\aaron\RiderProjects\Blazor-Social-Media\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\aaron\source\repos\SocialMediaApplication\_Imports.razor"
+#line 7 "C:\Users\aaron\RiderProjects\Blazor-Social-Media\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\aaron\source\repos\SocialMediaApplication\_Imports.razor"
+#line 8 "C:\Users\aaron\RiderProjects\Blazor-Social-Media\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\aaron\source\repos\SocialMediaApplication\_Imports.razor"
+#line 9 "C:\Users\aaron\RiderProjects\Blazor-Social-Media\_Imports.razor"
 using SocialMediaApplication;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\aaron\source\repos\SocialMediaApplication\_Imports.razor"
+#line 10 "C:\Users\aaron\RiderProjects\Blazor-Social-Media\_Imports.razor"
 using SocialMediaApplication.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\aaron\source\repos\SocialMediaApplication\Pages\FetchData.razor"
+#line 3 "C:\Users\aaron\RiderProjects\Blazor-Social-Media\Pages\FetchData.razor"
 using SocialMediaApplication.Data;
 
 #line default
@@ -98,7 +98,7 @@ using SocialMediaApplication.Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 39 "C:\Users\aaron\source\repos\SocialMediaApplication\Pages\FetchData.razor"
+#line 39 "C:\Users\aaron\RiderProjects\Blazor-Social-Media\Pages\FetchData.razor"
        
     private WeatherForecast[] forecasts;
 

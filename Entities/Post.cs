1d6d15cf9e2cb1b7d21254e01d2e300bdf029645
Aaron van den Berg <a.vdberg98@gmail.com>
2021-03-18 14:11:29 +0100

    Added two new projects (for CosmosDB)

---
+++ Entities/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+  public class Post
+  {
+    [Key]
+    public Guid PostId { get; set; }
+
+    public string Title { get; set; }
+
+    public string Content { get; set; }
+
+    public User User { get; set; }
+
+  }
+}
---
+++ Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entities
+{
+  public class User
+  {
+    public Guid UserId { get; set; }
+
+    public string Name { get; set; }
+
+    public ICollection<Post> Posts { get; set; }
+  }
+}

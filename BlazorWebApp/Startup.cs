a7bec50c0adce3055a3979bb1ec2d12268b606e2
Aaron van den Berg <a.vdberg98@gmail.com>
2021-03-18 15:05:40 +0100

    Fixed NuGet Dependencies

--- BlazorWebApp/Startup.cs
+++ BlazorWebApp/Startup.cs
@@ -29,6 +29,7 @@ namespace SocialMediaApplication
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<UserService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

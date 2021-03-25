e0ad4c6aa32c0cfbaee88d8dc69e97ec6a219f56
Joris <jorisnijkamp@gmail.com>
2021-03-25 14:36:55 +0100

    àsdf

--- BlazorWebApp/Startup.cs
+++ BlazorWebApp/Startup.cs
@@ -39,15 +39,14 @@ namespace SocialMediaApplication
                 options.ClaimActions.MapJsonKey("urn:google:profile", "link");
                 options.ClaimActions.MapJsonKey("urn:google:image", "picture");
             });
+            
             // From: https://github.com/aspnet/Blazor/issues/1554
             // Adds HttpContextAccessor
             // Used to determine if a user is logged in
             // and what their username is
             services.AddHttpContextAccessor();
             services.AddScoped<HttpContextAccessor>();
-            // Required for HttpClient support in the Blazor Client project
-            services.AddHttpClient();
-            services.AddScoped<HttpClient>();
+            
             // Pass settings to other components
             services.AddSingleton<IConfiguration>(Configuration);
         }

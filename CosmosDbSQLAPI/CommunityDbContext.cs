d60a50eb735f597f258fe4f3ee252999a8180a54
Aaron van den Berg <a.vdberg98@gmail.com>
2021-03-26 10:49:04 +0100

    Structure updated

--- CosmosDbSQLAPI/CommunityDbContext.cs
+++ CosmosDbSQLAPI/CommunityDbContext.cs
@@ -5,42 +5,37 @@ using Microsoft.Extensions.Logging;
 
 namespace CosmosDbSQLAPI
 {
-  public class CommunityDbContext : DbContext
-  {
-    public DbSet<User> Users { get; set; }
-
-    public DbSet<Post> Posts { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    public class CommunityDbContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
 
-      var Endpoint = "https://localhost:8081";
-      var Key = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var Endpoint = "https://localhost:8081";
+            var Key = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
 
-      optionsBuilder.UseCosmos(
-                               Endpoint,
-                               Key,
-                                "CommunityDatabase")
-                               .UseLoggerFactory(GenerateLoggerFactory())
-                               .EnableSensitiveDataLogging(true);
+            optionsBuilder.UseCosmos(Endpoint, Key, "CommunityDatabase")
+                .UseLoggerFactory(GenerateLoggerFactory())
+                .EnableSensitiveDataLogging(true);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToContainer("Users");
+            modelBuilder.Entity<User>().OwnsMany(s => s.Posts);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        private ILoggerFactory GenerateLoggerFactory()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            //Query debugger / logger
+            serviceCollection.AddLogging(builder =>
+                builder.AddConsole().AddFilter(DbLoggerCategory.Database.Command.Name, LogLevel.Trace));
+
+            return serviceCollection.BuildServiceProvider().GetService<ILoggerFactory>();
+        }
     }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-      modelBuilder.Entity<User>().ToContainer("Users");
-      modelBuilder.Entity<User>().OwnsMany(s => s.Posts);
-      base.OnModelCreating(modelBuilder);
-    }
-
-    private ILoggerFactory GenerateLoggerFactory()
-    {
-      var serviceCollection = new ServiceCollection();
-      
-      //Query debugger / logger
-      serviceCollection.AddLogging(builder => builder.AddConsole().AddFilter(DbLoggerCategory.Database.Command.Name, LogLevel.Trace));
-
-      return serviceCollection.BuildServiceProvider().GetService<ILoggerFactory>();
-    }
-
-  }
 }
